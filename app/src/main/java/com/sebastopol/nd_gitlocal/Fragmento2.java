e995c1e8f5790dc84021439eba1f100d151c3ab7
Sebastopol <requenacva@gmail.com>
2022-03-05 03:17:01 -0300

    i.2_p:master update v 0.1

--- app/src/main/java/com/sebastopol/nd_gitlocal/Fragmento2.java
+++ app/src/main/java/com/sebastopol/nd_gitlocal/Fragmento2.java
@@ -1,4 +1,37 @@
 package com.sebastopol.nd_gitlocal;
 
-public class Fragmento2 {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragmento2 extends Fragment {
+
+    Button btnSave;
+
+   @Override
+    public View onCreateView(LayoutInflater, ViewGroup container,
+                             Bundle saveInstanceStance){
+
+       View v = inflater.inflate(R.id.btnSave,container, false);
+
+       btnSave = v.findViewById(R.id.btnSave);
+
+       btnSave.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Toast.makeText(getContext(),"Salvaste al planeta Tierra", Toast.LENGTH_LONG).show();
+           }
+       });
+
+       return v;
+
+
+
+    }
+
 }
